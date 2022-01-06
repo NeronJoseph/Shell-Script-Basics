9751b57e40f673b4d0d036cfd256cf0766ceb3b4
Neron Joseph <njoseph@menervasoftware.com>
2022-01-07 00:20:16 +0530

    Added some sample usecases for head command

---
+++ head-tail-commands/head.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "\n\nPrinting the first 10 lines"
+head testfile.txt
+
+echo "\n\nPrinting the first 15 lines"
+head -n 15 testfile.txt
+
+echo "\n\nExclude the last 10 lines from the file"
+head -n -10 testfile.txt
+
+echo "\n\nPrint the first 20 characters"
+head -c 20 testfile.txt
+
+echo "\n\nExclude the last 20 characters"
+head -c -20 testfile.txt
