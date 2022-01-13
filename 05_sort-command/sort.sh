c90c29e2329e57f4877ad0c4feefcc2629256d57
Neron Joseph <njoseph@menervasoftware.com>
2022-01-14 01:46:58 +0530

    Reordered the linux command. Moved sort command to position 5.

---
+++ 05_sort-command/sort.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Simply sort the sample.txt file
+sort sample.txt
+
+#Sort the file content in reverse lexicographical order
+sort -r sample.txt
+
+#Sort the file 'number-samples.txt' that contain numerical values
+sort -n number-samples.txt
+
+#Sort the file 'number-samples.txt' that contain numerical values in descending order
+sort -nr number-samples.txt
+
+# Sort the file 'samplefile-2.txt' with the second column and also in the descending order
+sort -t$'\t' -k2 -nr samplefile-2.txt
+
+# Sort the file 'samplefile-3.txt' with the second column and also in the descending order. Here, the delimiter is | (pipe)
+sort -t '|' -k2 -nr samplefile-3.txt
